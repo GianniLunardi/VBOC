41481089ee5767be0c9be2a609f1b117a93537d3
GianniLunardi <gianni.lunardi96@gmail.com>
2024-11-07 16:49:21 +0100

    update parsing (activation) and parameters

--- src/vboc/parser.py
+++ src/vboc/parser.py
@@ -6,22 +6,24 @@ import argparse
 
 def parse_args():
     parser = argparse.ArgumentParser()
-    parser.add_argument('-s', '--system', type=str, default='double_pendulum',
-                        help='Systems to test. Available: pendulum, double_pendulum, ur5, z1')
-    parser.add_argument('--dofs', type=int, default=False, nargs='?',
+    parser.add_argument('-s', '--system', type=str, default='z1',
+                        help='Systems to test. Available: pendulum, double_pendulum, triple_pendulum, ur5, z1')
+    parser.add_argument('-d', '--dofs', type=int, default=False, nargs='?',
                         help='Number of desired degrees of freedom of the system')
     parser.add_argument('-b', '--build', action='store_true',
                         help='Build the code of the embedded controller')
     parser.add_argument('-v', '--vboc', action='store_true',
                         help='Compute data on border of the viability kernel')
-    parser.add_argument('--horizon', type=int, default=100,
+    parser.add_argument('--horizon', type=int, default=45,
                         help='Horizon of the optimal control problem')
     parser.add_argument('-t', '--training', action='store_true',
                         help='Train the neural network model that approximates the viability kernel')
     parser.add_argument('-p', '--plot', action='store_true',
                         help='Plot the approximated viability kernel')
-    parser.add_argument('--epochs', type=int, default=1000,
+    parser.add_argument('-e', '--epochs', type=int, default=1000,
                         help='Number of epochs for training the neural network')
+    parser.add_argument('-a', '--activation', type=str, default='relu',
+                        help='Activation function for the neural network')
     return vars(parser.parse_args())
 
 
@@ -49,7 +51,7 @@ class Parameters:
         self.cpu_num = int(parameters['cpu_num'])
         self.build = False
         
-        self.T = float(parameters['T'])
+        self.N = int(parameters['N'])
         self.dt = float(parameters['dt'])
         self.alpha = int(parameters['alpha'])
 
@@ -57,13 +59,11 @@ class Parameters:
         self.solver_mode = parameters['solver_mode']
         self.nlp_max_iter = int(parameters['nlp_max_iter'])
         self.qp_max_iter = int(parameters['qp_max_iter'])
-        self.nlp_tol_stat = float(parameters['nlp_tol_stat'])
         self.alpha_reduction = float(parameters['alpha_reduction'])
         self.alpha_min = float(parameters['alpha_min'])
         self.levenberg_marquardt = float(parameters['levenberg_marquardt'])
 
         self.state_tol = float(parameters['state_tol'])
-        # self.conv_tol = float(parameters['conv_tol'])
         self.cost_tol = float(parameters['cost_tol'])
         self.globalization = 'MERIT_BACKTRACKING'
 
@@ -72,16 +72,17 @@ class Parameters:
         self.beta = float(parameters['beta'])
         self.train_ratio = float(parameters['train_ratio'])
         self.val_ratio = float(parameters['val_ratio'])
+        self.hidden_size = int(parameters['hidden_size'])
+        self.hidden_layers = int(parameters['hidden_layers'])
 
         # For cartesian constraint
         self.obs_flag = bool(parameters['obs_flag'])
         if urdf_name == 'double_pendulum':
             frame_name = 'link2' 
+        elif urdf_name == 'triple_pendulum':
+            frame_name = 'link3'
         elif urdf_name == 'z1':
             frame_name = 'gripperMover'
         else:
             frame_name = 'none'
         self.frame_name = frame_name       #  TODO: dependence on the robot
-
-        # Payload 
-        self.payload = bool(parameters['payload'])
